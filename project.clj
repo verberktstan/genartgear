d82e9df6aa5d38377393ec3f7ce7caab39e51b91
Stan Verberkt <verberktstan@gmail.com>
2023-09-13 11:27:28 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject genartgear "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[genartlib/genartlib "0.1.25"]
+                 [org.clojure/clojure "1.10.1"]
+                 [quil/quil "3.1.0"]])
