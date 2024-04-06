da10bd5dc623fcd9add034216ccfa10620910db2
Stan Verberkt <2913270+verberktstan@users.noreply.github.com>
2024-04-06 09:31:03 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# genartgear
+
+Some functions to ease the usage of Quil for Generative Art.
+
+## Usage
+
+Create a Jar with `lein uberjar` and copy the (non-standalone) jar to the :resource-paths of another Quil project.
+
+## License
+
+Copyright © 2016 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject genartgear "0.1.3-SNAPSHOT"
+  :description "Genart Gear - A library with utilitys for generative artwork in Quil."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[genartlib/genartlib "0.1.25"]
+                 [org.clojure/clojure "1.10.1"]
+                 [quil/quil "3.1.0"]])
