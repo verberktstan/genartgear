ae6169a6d84eabe1b1476532dc54e6cc536a4d83
Stan Verberkt <verberktstan@gmail.com>
2024-04-14 19:41:05 +0200

    Extend collision? to support point-to-line distance as well

--- src/genartgear/util.clj
+++ src/genartgear/util.clj
@@ -1,10 +1,22 @@
 (ns genartgear.util
-  (:require [genartlib.util :as u]
+  (:require [genartlib.algebra :as a]
+            [genartlib.util :as u]
             [quil.core :as q]))
+
+(defn- point?
+  [& numbers]
+  (every? number? numbers))
+
+(defn- line?
+  [& points]
+  (every? point? points))
 
 (defn collision?
   "Returns a function that returns true if point collides with another point, with respect to the margin."
   ([point] (collision? point (u/w 1/100)))
   ([point margin]
-   (fn [another-point]
-     (< (apply q/dist (concat point another-point)) margin))))
+   (fn [[a b :as line-or-point]]
+     (cond (line? line-or-point)
+           (< (a/point-to-line-dist a b point) margin)
+           (point? line-or-point)
+           (< (a/point-dist point line-or-point) margin)))))
