471ccd967db18261cdc7809fe3b5e0b7dcdbc347
Stan Verberkt <verberktstan@gmail.com>
2024-05-14 13:34:46 +0200

    Fix util ns by removing collision?

--- src/genartgear/util.clj
+++ src/genartgear/util.clj
@@ -1,6 +1,4 @@
-(ns genartgear.util
-  (:require [genartlib.algebra :as a]
-            [genartlib.util :as u]))
+(ns genartgear.util)
 
 (defn point?
   [point]
@@ -10,16 +8,6 @@
   [line]
   (and (every? coll? line)
        (every? point? line)))
-
-(defn collision?
-  "Returns a function that returns true if point collides with another point, with respect to the margin."
-  ([point line-or-point]
-   (collision? point (u/w 1/100) line-or-point))
-  ([point margin [a b :as line-or-point]]
-   (cond (line? line-or-point)
-         (< (a/point-to-line-dist a b point) margin)
-         (point? line-or-point)
-         (< (a/point-dist point line-or-point) margin))))
 
 (defn clip
   "Returns a function that returns a clipped value between lo and hi. When input
