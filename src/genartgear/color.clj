404a541be96efd2a28b74b5502cf416cf57bf98c
Stan Verberkt <2913270+verberktstan@users.noreply.github.com>
2024-05-14 06:43:55 +0200

    Add fluorescent tints to colors (#5)
    * Add fluorescent red tints to colors
    
    * Add yellow, green fluo tints
    
    * Add magenta fluo tints

--- src/genartgear/color.clj
+++ src/genartgear/color.clj
@@ -16,10 +16,20 @@
 (def CRAYOLA
   (merge
     #:crayola{:vivid-violet [289.21, 61.81, 56.47, 1]}
-    #:fluorescent{:outrageous-orange [12.3, 78.43, 100, 1]
+    #:fluorescent{:atomic-tangerine  [20, 60, 100, 1]
+                  :electric-lime     [72, 100, 100, 1]
+                  :outrageous-orange [12.3, 78.43, 100, 1]
                   :radical-red       [347.8, 79.22, 100, 1]
                   :blizzard-blue     [195.6, 65.22, 90.2, 1]
-                  :screamin-green    [120, 60, 100, 1]}
+                  :hot-magenta       [312, 100, 100, 1]
+                  :laser-lemon       [60, 60, 100, 1]
+                  :magic-mint        [153.43, 29.17, 94.12, 1]
+                  :neon-carrot       [30, 80, 100, 1]
+                  :razzle-dazzle-rose [309.03, 78.15, 93.33, 1]
+                  :screamin-green    [120, 60, 100, 1]
+                  :shocking-pink     [300, 56.86, 100, 1]
+                  :sunglow           [45, 80, 100, 1]
+                  :wild-watermelon   [349.26, 64.03, 99.22, 1]}
     #:bright{:red-salsa    [355.08, 77.08, 99.22, 1]
              :orange-soda  [9.52, 75.6, 98.04, 1]
              :denim-blue   [226.62, 81.32, 71.37, 1]
